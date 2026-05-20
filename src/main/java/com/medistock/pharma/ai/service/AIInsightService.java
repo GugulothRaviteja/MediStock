35ecba766ba5fdd3e86d22ad81650c6a71f20374
guguloth raviteja <gugulothraviteja111213@gmail.com>
2026-05-20 21:09:23 +0530

    2nd change on ai service module

--- src/main/java/com/medistock/pharma/ai/service/AIInsightService.java
+++ src/main/java/com/medistock/pharma/ai/service/AIInsightService.java
@@ -1,7 +1,5 @@
 package com.medistock.pharma.ai.service;
 
-
-
 import com.medistock.pharma.ai.dto.AIInsightResponse;
 import com.medistock.pharma.medicine.model.Medicine;
 import com.medistock.pharma.medicine.repository.MedicineRepository;
@@ -9,7 +7,6 @@ import com.medistock.pharma.sales.model.Sale;
 import com.medistock.pharma.sales.repository.SaleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 
